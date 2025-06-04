0da2c6927cb7d7a26baebb765a221182c5a3bf3e
Greg Rose <gvrose8192@gmail.com>
2025-06-03 23:13:53 -0700

    Add working database update
    If the db update flag is specified the new updateRateTable()
    function is called and if any new records from the current month
    are found they are added to the database.
    
    Additional cleanup of example code no longer needed.  Some other
    formatting and cleanup as well.

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"log"
 	"strconv"
 	"bufio"
-	"os"
 	"time"
+	"strings"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/gin-gonic/gin"
@@ -126,11 +126,161 @@ func getRateByID(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, rate)
 }
 
+func convertStringToFloat(number_string string) (float32, error) {
+	retVal64, err := strconv.ParseFloat(number_string, 64)
+	if err != nil {
+		return 0.0, err
+	}
+	retVal32 := float32(retVal64)
+	return retVal32, nil
+}
+
+func reverseStringArray(arr []string) []string {
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	return arr
+}
+
+func insertNewRate(rateString []string) error {
+	newRate := Rate{ID: 0, Date: rateString[0]}
+	fmt.Printf("New Record Date is %s\n", newRate.Date)
+
+	var err error = nil
+	newRate.one_month, err = convertStringToFloat(rateString[1])
+	if err != nil {
+		return err
+	}
+
+	newRate.one_5month, err = convertStringToFloat(rateString[2])
+	if err != nil {
+		return err
+	}
+
+	newRate.two_month, err = convertStringToFloat(rateString[3])
+	if err != nil {
+		return err
+	}
+
+	newRate.three_month, err = convertStringToFloat(rateString[4])
+	if err != nil {
+		return err
+	}
+
+	newRate.four_month, err = convertStringToFloat(rateString[5])
+	if err != nil {
+		return err
+	}
+
+	newRate.six_month, err = convertStringToFloat(rateString[6])
+	if err != nil {
+		return err
+	}
+
+	newRate.one_year, err = convertStringToFloat(rateString[7])
+	if err != nil {
+		return err
+	}
+
+	newRate.two_year, err = convertStringToFloat(rateString[8])
+	if err != nil {
+		return err
+	}
+
+	newRate.three_year, err = convertStringToFloat(rateString[9])
+	if err != nil {
+		return err
+	}
+
+	newRate.five_year, err = convertStringToFloat(rateString[10])
+	if err != nil {
+		return err
+	}
+
+	newRate.seven_year, err = convertStringToFloat(rateString[11])
+	if err != nil {
+		return err
+	}
+
+	newRate.ten_year, err = convertStringToFloat(rateString[12])
+	if err != nil {
+		return err
+	}
+
+	newRate.twenty_year, err = convertStringToFloat(rateString[13])
+	if err != nil {
+		return err
+	}
+
+	newRate.thirty_year, err = convertStringToFloat(rateString[14])
+	if err != nil {
+		return err
+	}
+
+
+	fmt.Printf("one month: %f\tone 1/2 month: %f\ttwo month: %f\tthree month: %f\t four_month: %f\n", newRate.one_month, newRate.one_5month, newRate.two_month, newRate.three_month, newRate.four_month)
+	fmt.Printf("six month: %f\tone year: %f\ttwo year: %f\tthree year: %f\tfive year: %f\n", newRate.six_month, newRate.one_year, newRate.two_year, newRate.three_year, newRate.five_year)
+	fmt.Printf("seven year: %f\tten year: %f\ttwenty year: %f\tthirty year: %f\n", newRate.seven_year, newRate.ten_year, newRate.twenty_year, newRate.thirty_year)
+
+	result, dberr := db.Exec("INSERT INTO rate(date, one_month, one_5month, two_month, three_month, four_month, six_month, one_year, two_year, three_year, five_year, seven_year, ten_year, twenty_year, thirty_year) 					 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", newRate.Date, newRate.one_month, newRate.one_5month, newRate.two_month, newRate.three_month, newRate.four_month, newRate.six_month, newRate.one_year, newRate.two_year, newRate.three_year, newRate.five_year, newRate.seven_year, newRate.ten_year, newRate.twenty_year, newRate.thirty_year)
+	if dberr != nil {
+		fmt.Printf("insert rate: %v\n", dberr)
+		return dberr
+	}
+	fmt.Println("Insert finished")
+
+	rowsAffected, dberr := result.RowsAffected()
+	if dberr != nil {
+		fmt.Printf("insert rate: %v\n", dberr)
+		return dberr
+	}
+	fmt.Printf("Rows Affected: %d\n", rowsAffected)
+
+	id, dberr := result.LastInsertId()
+	if dberr != nil {
+		fmt.Printf("get last id: %v\n", dberr)
+		return dberr
+	}
+
+	fmt.Printf("Created record with id %d\n", id)
+
+	return nil
+}
+
+func addNewRates(rateArray []string) error {
+	for i := 0; i < len(rateArray); i = i + 1 {
+		csvElements := strings.Split(rateArray[i], ",")
+		var rates []Rate
+		var rate Rate
+
+		fmt.Println(csvElements)
+		fmt.Printf("Checking for record with Date: %s\n", csvElements[0])
+
+		rows, err := db.Query("SELECT * FROM rate WHERE date like ?",  csvElements[0])
+		if err != nil {
+		 	return err
+		}
+		for rows.Next() {
+			if err := rows.Scan(&rate.ID, &rate.Date, &rate.one_month, &rate.one_5month, &rate.two_month, &rate.three_month, &rate.four_month, &rate.six_month, &rate.one_year, &rate.two_year, &rate.three_year, &rate.five_year, &rate.seven_year, &rate.ten_year, &rate.twenty_year, &rate.thirty_year); err != nil {
+				return err
+			}
+			rates = append(rates, rate)
+		}
+		rows.Close()
+		if len(rates) == 0 {
+			insertNewRate(csvElements)
+		}
+	}
+
+	return nil
+}
+
 func updateRateTable() {
+	newLines := []string{}
 	currentTime := time.Now()
 	f := currentTime.Format("200601")
 
-	s := fmt.Sprintf("https://home.treasury.gov/resource-center/data-chart-center/interest-rates/daily-treasury-rates.csv/all/202505?type=daily_treasury_yield_curve&field_tdr_date_value_month=%s&page&_format=csv", f)
+	s := fmt.Sprintf("https://home.treasury.gov/resource-center/data-chart-center/interest-rates/daily-treasury-rates.csv/all/%s?type=daily_treasury_yield_curve&field_tdr_date_value_month=%s&page&_format=csv", f, f)
 
 	resp, err := http.Get(s)
 	if err != nil {
@@ -142,7 +292,7 @@ func updateRateTable() {
 	scanner := bufio.NewScanner(resp.Body)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		newLines = append(newLines, line)
 	}
 
 	if scan_err := scanner.Err(); scan_err != nil {
@@ -150,35 +300,22 @@ func updateRateTable() {
 		return
 	}
 
+
+	// The Treasury delivers the CSV lines from highest to lowest date
+	// Reverse that and also drop the first line which is the CVS header fields
+	// Our database 'rate' table already defines those.
+	reversedLines := reverseStringArray(newLines)
+	newRateLines := reversedLines[:len(reversedLines) - 1]
+	addNewRates(newRateLines)
 }
 
-// addAlbum adds the specified album to the database,
-// returning the album ID of the new entry
-//func addAlbum(alb Album) (int64, error) {
-//	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
-//	if err != nil {
-//		return 0, fmt.Errorf("addAlbum: %v", err)
-//	}
-//	id, err := result.LastInsertId()
-//	if err != nil {
-//		return 0, fmt.Errorf("addAlbum: %v", err)
-//	}
-//	return id, nil
-//}
-
 func main() {
-
+	// Let's get started
 	username := flag.String("username", "your name", "a string")
 	password := flag.String("password", "your password", "a string")
 	db_update := flag.Bool("db-update", false, "Update database")
 
 	flag.Parse()
-
-	if *db_update {
-		updateRateTable()
-	}
-
-	os.Exit(0)
 
 	// Capture connection properties.
 	cfg := mysql.NewConfig()
@@ -200,39 +337,12 @@ func main() {
 		log.Fatal(pingErr)
 	}
 
-	rates, err := rateByDate("2025-22-05")
-
-	if err != nil {
-		log.Fatal(err)
+	if *db_update {
+		updateRateTable()
 	}
-	fmt.Printf("Rates found: %v\n", rates)
-
-	// Hard-code ID 3 here to test the query.
-	rate, err := rateByID(3)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Rate found: %v\n", rate)
-
 
 	router := gin.Default()
 	router.GET("/rates", rateGetAll)
 	router.GET("/rates/:id", getRateByID)
 	router.Run("localhost:8080")
-
-//	alb, err := albumByID(2)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Printf("Album found: %v\n", alb)
-//
-//	albID, err := addAlbum(Album{
-//		Title:  "The Modern Sound of Betty Carter",
-//		Artist: "Betty Carter",
-//		Price:  49.99,
-//	})
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Printf("ID of added album: %v\n", albID)
 }
